53227aef72730ffa77a403f7f2d3f05a4650098f
csanlag861 <csanlag861@g.educaand.es>
2026-01-16 13:00:57 +0100

    feat(backend-config): Configuración del proyecto, instalación de prisma, y configuración del Schema de ROL

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
+  const logger = new Logger('App - Auth');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // app.use(helmet());
+  app.use(cookieParser());
+
+  app.setGlobalPrefix('api');
+
+  app.enableCors({
+    origin: envs.ALLOWED_ORIGINS,
+    credentials: true, // Permite cookies
+    // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    // allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(envs.PORT);
+  logger.log('App running on PORT:');
 }
+
 bootstrap();
