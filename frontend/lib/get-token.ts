c2dc72fe709d22b07122e80d8b4e517f2d5ae6b4
csanlag861 <csanlag861@g.educaand.es>
2026-01-29 12:58:51 +0100

    feat(post): implement post

---
+++ frontend/lib/get-token.ts
@@ -0,0 +1,7 @@
+'use server';
+import { cookies } from "next/headers";
+
+export async function GetCookies () {
+    const cookieStore = await cookies();
+    return cookieStore.get("access_token")?.value;
+}
