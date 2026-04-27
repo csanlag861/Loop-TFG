0b295ee2024c72486e17b73c7df467f8e78d17e7
csanlag861 <csanlag861@g.educaand.es>
2026-04-27 12:45:24 +0200

    feat: gestion token

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,13 @@
 import stylesPage from "./page.module.css";
 import Form from "@/features/landing/Form/Form";
 import Cards from "@/features/landing/Cards/Cards";
+import { GetCookies } from "@/lib/get-token";
+import { homePath } from "@/utils/paths";
+import { redirect } from "next/navigation";
 
-export default function LandingPage() {
+export default async function LandingPage() {
+  const token = await GetCookies();
+  if (token) redirect(homePath());
   return (
     <main className={stylesPage.main}>
       <h1>Loop</h1>
