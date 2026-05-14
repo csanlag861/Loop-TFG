2fb3438593c40f9becbe34e3c66bc72fd9dc2167
csanlag861 <csanlag861@g.educaand.es>
2026-05-14 17:56:09 +0200

    fix paths

--- frontend/utils/paths.ts
+++ frontend/utils/paths.ts
@@ -1,8 +1,6 @@
 export const landingPath = () => "/";
 export const homePath = () => "/posts";
 
-export const searchPath = () => "/busqueda";
-
 export const bookmarkPath = () => "/guardados";
 
 export const profilePath = ({ param }: { param: number }) => `/perfil/${param}`;
