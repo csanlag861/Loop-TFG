1e180fbd6ab6958a22db551c02e614d05a1050d8
csanlag861 <csanlag861@g.educaand.es>
2026-05-14 19:04:25 +0200

    fix: api url

--- frontend/utils/api.ts
+++ frontend/utils/api.ts
@@ -1,7 +1,7 @@
 const isServer = typeof window === "undefined";
 const API_URL = process.env.API_URL || "http://looptfg-backend-o5hyfi:3000";
 
-const BASE_URL = isServer ? API_URL : "";
+const BASE_URL = API_URL;
 
 const prefix = "api/";
 const prefixPosts = "post/";
