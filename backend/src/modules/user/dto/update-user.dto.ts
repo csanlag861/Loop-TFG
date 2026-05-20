4c00c5a5a3ad84f3531914c74c92a32181e4baa4
csanlag861 <csanlag861@g.educaand.es>
2026-05-20 19:31:49 +0200

    fix: passwordResetNonce error

--- backend/src/modules/user/dto/update-user.dto.ts
+++ backend/src/modules/user/dto/update-user.dto.ts
@@ -1,8 +1,11 @@
 import { PartialType, OmitType } from '@nestjs/mapped-types';
+import { IsOptional, IsString } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(
   OmitType(CreateUserDto, ['email'] as const),
 ) {
+  @IsOptional()
+  @IsString()
   passwordResetNonce?: string | null;
 }
